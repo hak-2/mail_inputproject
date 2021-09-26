e6813da08742dc5491c170cba491ce32e1b68ba8
unknown <tainka113@gmail.com>
2021-09-27 00:52:25 +0900

    import

---
+++ import/import/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
+
+from .models import Company
+
+class CompanyAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
+
+admin.site.register(Company, CompanyAdmin)
